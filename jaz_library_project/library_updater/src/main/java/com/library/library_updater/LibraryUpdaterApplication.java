e34b464ae6cc5baa364c0e620485be13822fe74a
stanoz <s.oziemczuk@gmail.com>
2024-01-01 15:21:23 +0100

    added updater module

---
+++ jaz_library_project/library_updater/src/main/java/com/library/library_updater/LibraryUpdaterApplication.java
@@ -0,0 +1,13 @@
+package com.library.library_updater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryUpdaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryUpdaterApplication.class, args);
+    }
+
+}
---
+++ jaz_library_project/library_updater/src/test/java/com/library/library_updater/LibraryUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.library_updater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryUpdaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
